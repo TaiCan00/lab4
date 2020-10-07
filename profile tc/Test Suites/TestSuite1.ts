9e1f9763f354d49cc1321aaf9045818ceb7022bf
taicancr7 <taicancr7@gmail.com>
2020-10-07 19:54:33 +0700

    update

---
+++ profile tc/Test Suites/TestSuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1842db2a-9f5c-4b05-9dc4-46848a203afa</testSuiteGuid>
+   <testCaseLink>
+      <guid>e8f3f3e4-708b-41d8-bb02-dd80392f45e5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc_01_profile</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
